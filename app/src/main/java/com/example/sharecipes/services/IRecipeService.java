9845b80c2d77ea10e467f4f49ee4a3a35b38be01
Nadav Bar Lev <nadav.barlev@moovit.com>
2019-04-27 00:42:29 +0300

    Retrofit Recipe Interface

--- app/src/main/java/com/example/sharecipes/services/IRecipeService.java
+++ app/src/main/java/com/example/sharecipes/services/IRecipeService.java
@@ -1,4 +1,24 @@
 package com.example.sharecipes.services;
 
+import com.example.sharecipes.services.responses.RecipeResponse;
+import com.example.sharecipes.services.responses.RecipeSearchResponse;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
 public interface IRecipeService {
+
+    @GET("api/get")
+    Call<RecipeResponse> getRecipe(
+            @Query("key") String key,
+            @Query("rId") String recipe_id
+    );
+
+    @GET("api/search")
+    Call<RecipeSearchResponse> searchRecipe(
+            @Query("key") String key,
+            @Query("q") String query,
+            @Query("page") String page
+    );
 }
