662a53fb8bfdf844180c5cf3372059741602d5a5
Nadav Bar Lev <nadav.barlev@moovit.com>
2019-06-08 22:47:16 +0300

    NetworkBoundResource - Implement single source of truth principal

--- app/src/main/java/com/example/sharecipes/util/AppExecutors.java
+++ app/src/main/java/com/example/sharecipes/util/AppExecutors.java
@@ -1,5 +1,9 @@
 package com.example.sharecipes.util;
 
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 
@@ -9,15 +13,20 @@ public class AppExecutors {
     private static final String TAG = "AppExecutors";
 
     /* Data Members */
-    private static AppExecutors instance = null;
-    private final ScheduledExecutorService mNetworkIO;
+    private final Executor mBackgroundThread;
+    private final Executor mMainThread;
+
+    // DO NOT NEED ANOTHER BACKGROUND THREAD TO DO THE NETWORK REQUEST BECAUSE
+    // LIVE DATA AUTOMATICALLY DOES THEM ASYNC
 
     /* Constructor */
     private AppExecutors() {
-        mNetworkIO = Executors.newScheduledThreadPool(3);
+        mBackgroundThread = Executors.newSingleThreadExecutor();
+        mMainThread = new MainThreadExecutor();
     }
 
     /* Singleton */
+    private static AppExecutors instance = null;
     public static AppExecutors getInstance() {
         if (instance == null) {
             instance = new AppExecutors();
@@ -25,8 +34,21 @@ public class AppExecutors {
         return instance;
     }
 
-    /* Methods */
-    public ScheduledExecutorService networkIO() {
-        return mNetworkIO;
+    /* Getter and Setter */
+    public Executor background() {
+        return mBackgroundThread;
+    }
+
+    public Executor main() {
+        return mMainThread;
+    }
+
+    /* Class - MainThreadExecutor */
+    private class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+        @Override
+        public void execute(Runnable command) {
+            mainThreadHandler.post(command);
+        }
     }
 }
--- app/src/main/java/com/example/sharecipes/util/Network/NetworkBoundResource.java
+++ app/src/main/java/com/example/sharecipes/util/Network/NetworkBoundResource.java
@@ -2,19 +2,165 @@ package com.example.sharecipes.util.Network;
 
 import android.arch.lifecycle.LiveData;
 import android.arch.lifecycle.MediatorLiveData;
+import android.arch.lifecycle.Observer;
 import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.WorkerThread;
 
+import com.example.sharecipes.util.AppExecutors;
+
+import okhttp3.Response;
+
 /**
- * Implement Single Source of Truth Principal - The Data Base Cache
+ * Implement Single Source of Truth Principal
  **/
 
 public abstract class NetworkBoundResource<CacheObject, RequestObject> {
 
-    // The Single Source of Truth
-    private MediatorLiveData<Resource<CacheObject>> results = new MediatorLiveData<>();
+    /* Data Members */
+
+    private AppExecutors executors;
+    private MediatorLiveData<Resource<CacheObject>> results; // The Single Source of Truth
+
+    /* Constructor*/
+    NetworkBoundResource(AppExecutors executors) {
+        this.executors = executors;
+        this.results = new MediatorLiveData<>();
+        setup();
+    }
+
+    /* Methods */
+    public void setup() {
+
+        // Update LiveData for Loading Status
+        results.setValue((Resource<CacheObject>)Resource.loading(null));
+
+        // Observe LiveData source from local DB
+        final LiveData<CacheObject> cacheFromDB = loadFromDb();
+
+        results.addSource(cacheFromDB, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(@Nullable CacheObject cacheObject) {
+
+                // Stop Observing
+                results.removeSource(cacheFromDB);
+
+                // Fetch from network if needed
+                if (shouldFetch(cacheObject)) {
+                    fetchFromNetwork(cacheFromDB);
+                }
+                else {
+                    results.addSource(cacheFromDB, new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    private void fetchFromNetwork(final LiveData<CacheObject> cacheFromDB) {
+
+        // Update LiveData for Loading Status
+        results.addSource(cacheFromDB, new Observer<CacheObject>() {
+            @Override
+            public void onChanged(@Nullable CacheObject cacheObject) {
+                setValue(Resource.loading(cacheObject));
+            }
+        });
+
+        final LiveData<ApiResponse<RequestObject>> apiResponseLiveData = createCall();
+
+        results.addSource(apiResponseLiveData, new Observer<ApiResponse<RequestObject>>() {
+            @Override
+            public void onChanged(@Nullable ApiResponse<RequestObject> requestObjectApiResponse) {
+                results.removeSource(cacheFromDB);
+                results.removeSource(apiResponseLiveData);
+                handleNetworkResponse(requestObjectApiResponse);
+            }
+        });
+    }
+
+    private void handleNetworkResponse(final ApiResponse<RequestObject> requestObjectApiResponse) {
+
+        // Success
+        if (requestObjectApiResponse instanceof ApiResponse.ApiSuccessResponse) {
+
+            // Save to the local DB
+            AppExecutors.getInstance().background().execute(new Runnable() {
+                @Override
+                public void run() {
+                   RequestObject requestObject = (RequestObject)
+                           ((ApiResponse.ApiSuccessResponse)requestObjectApiResponse).getBody();
+                    saveCallResult(requestObject);
+
+                    // Start observing again to see the refreshed data
+                    AppExecutors.getInstance().main().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            results.addSource(loadFromDb(), new Observer<CacheObject>() {
+                                @Override
+                                public void onChanged(@Nullable CacheObject cacheObject) {
+                                    setValue(Resource.success(cacheObject));
+                                }
+                            });
+                        }
+                    });
+                }
+            });
+        }
+
+        // Empty
+        else if (requestObjectApiResponse instanceof ApiResponse.ApiEmptyResponse) {
+
+            AppExecutors.getInstance().main().execute(new Runnable() {
+                @Override
+                public void run() {
+                    results.addSource(loadFromDb(), new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.success(cacheObject));
+                        }
+                    });
+                }
+            });
+        }
+
+        // Error
+        else if (requestObjectApiResponse instanceof ApiResponse.ApiErrorResponse) {
+
+            final ApiResponse.ApiErrorResponse apiErrorResponse =
+                    (ApiResponse.ApiErrorResponse)requestObjectApiResponse;
+            AppExecutors.getInstance().main().execute(new Runnable() {
+                @Override
+                public void run() {
+                    results.addSource(loadFromDb(), new Observer<CacheObject>() {
+                        @Override
+                        public void onChanged(@Nullable CacheObject cacheObject) {
+                            setValue(Resource.error(apiErrorResponse.getErrorMsg(), cacheObject));
+                        }
+                    });
+                }
+            });
+        }
+    }
+
+    private void setValue(Resource<CacheObject> newValue) {
+        if (results.getValue() != newValue) {
+            results.setValue(newValue);
+        }
+    }
+
+    // Returns a LiveData object that represents the resource that's implemented
+    // in the base class.
+    public final LiveData<Resource<CacheObject>> getAsLiveData(){
+        return results;
+    };
+
+    /* Abstract Methods */
 
     // Called to save the result of the API response into the database.
     @WorkerThread
@@ -33,10 +179,4 @@ public abstract class NetworkBoundResource<CacheObject, RequestObject> {
     @NonNull
     @MainThread
     protected abstract LiveData<ApiResponse<RequestObject>> createCall();
-
-    // Returns a LiveData object that represents the resource that's implemented
-    // in the base class.
-    public final LiveData<Resource<CacheObject>> getAsLiveData(){
-        return results;
-    };
 }
